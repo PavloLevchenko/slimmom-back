203dc7ad27ca9e9b9e52df633bc1ae5e6ebb8311
PavloLevchenko <teasetrnet@gmail.com>
2023-06-19 22:14:12 +0200

    const createCookie method

--- helpers/createCookie.js
+++ helpers/createCookie.js
@@ -1,15 +1,14 @@
 const { cookieName, expiresCookieTime } = require('./constants');
 
-const DEV_MODE = process.env.NODE_ENV === 'development';
-
 const createCookie = (res, token) => {
   try {
     const expires = new Date(Date.now() + expiresCookieTime * 60 * 1000);
     res.cookie(cookieName, token, {
       signed: true,
       expires,
-      sameSite: DEV_MODE ? 'none' : true,
-      secure: !DEV_MODE,
+      httpOnly: true,
+      sameSite: 'none',
+      secure: true,
     });
   } catch (error) {
     console.error(error);
