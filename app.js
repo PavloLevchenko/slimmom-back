0f2b22d56c8a69aadaff8c9b7a3bacfb79a4285f
PavloLevchenko <teasetrnet@gmail.com>
2023-06-19 20:24:56 +0200

    updating cors

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
 const path = require('path');
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
+
+const LOCAL_URL = process.env.LOCAL_URL;
+const PUBLIC_URL = process.env.PUBLIC_URL;
+const DEVELOPMENT = process.env.NODE_ENV === 'development';
 
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
@@ -13,12 +17,12 @@ const { handleErrors } = require('./middleware');
 const app = express();
 const cookieSecret = process.env.COOKIE_SECRET;
 app.use(cookieParser(cookieSecret));
-const formatsLogger = process.env.NODE_ENV === 'development' ? 'dev' : 'short';
+const formatsLogger = DEVELOPMENT ? 'dev' : 'short';
 
 app.use(logger(formatsLogger));
 app.use(
   cors({
-    origin: true,
+    origin: DEVELOPMENT ? LOCAL_URL : PUBLIC_URL,
     credentials: true,
   })
 );
