099c2e2863822f7db2828e9cb986b0480177fa0a
PavloLevchenko <teasetrnet@gmail.com>
2023-06-19 20:49:05 +0200

    Revert "updating cors"
    This reverts commit 0f2b22d56c8a69aadaff8c9b7a3bacfb79a4285f.

--- app.js
+++ app.js
@@ -2,11 +2,7 @@ const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
 const path = require('path');
-const cookieParser = require('cookie-parser');
-
-const LOCAL_URL = process.env.LOCAL_URL;
-const PUBLIC_URL = process.env.PUBLIC_URL;
-const DEVELOPMENT = process.env.NODE_ENV === 'development';
+const cookieParser = require('cookie-parser')
 
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
@@ -17,12 +13,12 @@ const { handleErrors } = require('./middleware');
 const app = express();
 const cookieSecret = process.env.COOKIE_SECRET;
 app.use(cookieParser(cookieSecret));
-const formatsLogger = DEVELOPMENT ? 'dev' : 'short';
+const formatsLogger = process.env.NODE_ENV === 'development' ? 'dev' : 'short';
 
 app.use(logger(formatsLogger));
 app.use(
   cors({
-    origin: DEVELOPMENT ? LOCAL_URL : PUBLIC_URL,
+    origin: true,
     credentials: true,
   })
 );
