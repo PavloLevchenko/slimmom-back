d8697bbb0dbace045d2e417ca968ec098b8666e9
PavloLevchenko <teasetrnet@gmail.com>
2023-06-22 00:40:46 +0200

    transfering cookie name and time to env

--- helpers/createCookie.js
+++ helpers/createCookie.js
@@ -1,9 +1,10 @@
-const { cookieName, expiresCookieTime } = require('./constants');
+const COOKIE_NAME = process.env.COOKIE_NAME;
+const COOKIE_TIME = process.env.COOKIE_TIME; // minutes
 
 const createCookie = (res, token) => {
   try {
-    const expires = new Date(Date.now() + expiresCookieTime * 60 * 1000);
-    res.cookie(cookieName, token, {
+    const expires = new Date(Date.now() + COOKIE_TIME * 60 * 1000);
+    res.cookie(COOKIE_NAME, token, {
       signed: true,
       expires,
       httpOnly: true,
